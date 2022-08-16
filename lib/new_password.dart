88a4bc06b0349887aca455f97717221f259ee0ff
slothtae <jerrbro.333@gmail.com>
2022-08-16 12:40:24 +0530

    Forgot Password

--- lib/new_password.dart
+++ lib/new_password.dart
@@ -1,116 +1,121 @@
 import 'package:flutter/material.dart';
 
-class NewPassword extends StatelessWidget{
-  const NewPassword({Key? key}): super(key : key);
-  @override	
-  Widget build(BuildContext context){
+class NewPassword extends StatelessWidget {
+  const NewPassword({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body :Container(
+      body: Container(
         height: MediaQuery.of(context).size.height,
-	width: MediaQuery.of(context).size.width,
-	decoration: const BoxDecoration(
-	  gradient: LinearGradient(
-	    begin:Alignment.centerRight,
-	    end:Alignment.bottomLeft,
-	    colors:[
-	      Color(0xFFEF9A94), // a gradient touch to the screen background
-	      Color(0xFFFFEBEE),
-	    ],	    
-	  ),
-	),
-	child:Padding(
-	  padding: EdgeInsets.only(top:100,right:40,left:40,bottom :100),
-  	  child:Column(
-	    //mainAxisAlignment: MainAxisAlignment.center, // so that they stay in mid of screen
-	    crossAxisAlignment: CrossAxisAlignment.start, // just so the child are aligned to left
-	    children: [ 
-	      Padding(
-	        padding:EdgeInsets.zero,
-		child: Text(
-		  'New Password',
-		  style:TextStyle(
-		    fontFamily: 'Playfair Display',
-  		    fontSize: 27,
-		    fontWeight: FontWeight.bold,		    
-		  ),
-		),
-	      ),
-	      SizedBox(
-		height: 80,		      
-	      ),
-	      Container(
-		padding: const EdgeInsets.symmetric(vertical:1),
-		decoration: BoxDecoration(
-                   color: Colors.red.shade50,
-                   borderRadius: BorderRadius.circular(7),
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFFEF9A94), // a gradient touch to the screen background
+              Color(0xFFFFEBEE),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(top: 100, right: 40, left: 40, bottom: 100),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center, // so that they stay in mid of screen
+            crossAxisAlignment: CrossAxisAlignment
+                .start, // just so the child are aligned to left
+            children: [
+              Padding(
+                padding: EdgeInsets.zero,
+                child: Text(
+                  'New Password',
+                  style: TextStyle(
+                    fontFamily: 'Playfair Display',
+                    fontSize: 27,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 1),
+                decoration: BoxDecoration(
+                  color: Colors.red.shade50,
+                  borderRadius: BorderRadius.circular(7),
                 ),
                 child: TextFormField(
                   decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
-                    hintText: "New Password",
-                    hintStyle: TextStyle(color: Colors.blueGrey, fontSize: 16,
-                    fontWeight: FontWeight.w400,),
-                    contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14)
-                  ),
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      hintText: "New Password",
+                      hintStyle: TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 14)),
                 ),
-	      ),
-	      SizedBox(
- 	        height:20,
-	      ),
-	      Container(
-		padding: const EdgeInsets.symmetric(vertical:1),
-		decoration: BoxDecoration(
-                   color: Colors.red.shade50,
-                   borderRadius: BorderRadius.circular(7),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 1),
+                decoration: BoxDecoration(
+                  color: Colors.red.shade50,
+                  borderRadius: BorderRadius.circular(7),
                 ),
                 child: TextFormField(
                   decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
-                    hintText: "Confirm Password",
-                    hintStyle: TextStyle(color: Colors.blueGrey, fontSize: 16,
-                    fontWeight: FontWeight.w400,),
-                    contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14)
-                  ),
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      hintText: "Confirm Password",
+                      hintStyle: TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 14)),
                 ),
-	      ),
-	      SizedBox(
-		height: 40,		     
-	      ),
-	      SizedBox(
-                height: 50, 
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                height: 50,
                 width: MediaQuery.of(context).size.width,
                 child: ElevatedButton(
-                  onPressed: (){},
+                  onPressed: () {},
                   style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Color(0xff4A148C)),
-    	            shape: MaterialStateProperty
-                          .all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(7)
-                            ),
-                          ),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Color(0xff4A148C)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7)),
+                    ),
                   ),
                   child: const Text(
                     "Continue",
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 18, 
+                      fontSize: 18,
                       fontFamily: "PlayFair Display",
-                      ),
-                    ),  
-                  ),  
+                    ),
+                  ),
                 ),
-
-	    ],
-	  ),	  
-	),	
-      ),		    
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
