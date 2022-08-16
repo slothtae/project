a1a9c7b9196fdbd369b6d3fa81672936d4e002d4
slothtae <jerrbro.333@gmail.com>
2022-08-16 12:14:59 +0530

    Forgot Password screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -2,34 +2,35 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:terra_vision/register_screen.dart';
 import 'package:terra_vision/home/home_screen_normal_user.dart';
-
+import 'package:terra_vision/password.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    TextStyle defaultStyle= const TextStyle(color: Colors.black, fontSize: 14, fontFamily: "Playfair Display");
-    TextStyle linkStyle= const TextStyle(color:Colors.blue, fontSize: 14, fontFamily: "Playfair Display");
-    return  Scaffold(
+    TextStyle defaultStyle = const TextStyle(
+        color: Colors.black, fontSize: 14, fontFamily: "Playfair Display");
+    TextStyle linkStyle = const TextStyle(
+        color: Colors.blue, fontSize: 14, fontFamily: "Playfair Display");
+    return Scaffold(
       body: Container(
         height: MediaQuery.of(context).size.height,
-        decoration:  const BoxDecoration(
-            gradient:LinearGradient(
-                begin: Alignment.centerRight,
-                end: Alignment.centerLeft,
-                colors:[
-                  Color(0xFFEF9A94),
-                  Color(0xFFFFEBEE),
-                ]
-            ),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.centerRight,
+              end: Alignment.centerLeft,
+              colors: [
+                Color(0xFFEF9A94),
+                Color(0xFFFFEBEE),
+              ]),
         ),
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.only(top:100, left: 40, right: 40),
+            padding: const EdgeInsets.only(top: 100, left: 40, right: 40),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children:  [
+              children: [
                 const Text(
                   'Login',
                   style: TextStyle(
@@ -37,30 +38,35 @@ class LoginScreen extends StatelessWidget {
                     color: Colors.black,
                     fontSize: 27,
                     fontWeight: FontWeight.bold,
-
                   ),
                 ),
                 const SizedBox(
-                  height:8,
+                  height: 8,
                 ),
                 RichText(
                     text: TextSpan(
-                      style: defaultStyle,
-                      children: [
-                        const TextSpan(text: "Don't have an account?"),
-                        TextSpan(text:" Register",
-                          style: linkStyle,
-                          recognizer: TapGestureRecognizer()..onTap=(){
-                            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder:(context)=>RegisterScreen()), (route)=>true);
-                          },
-                        ),
-                      ],
-                    )),
+                  style: defaultStyle,
+                  children: [
+                    const TextSpan(text: "Don't have an account?"),
+                    TextSpan(
+                      text: " Register",
+                      style: linkStyle,
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegisterScreen()),
+                              (route) => true);
+                        },
+                    ),
+                  ],
+                )),
                 const SizedBox(
-                  height:30,
+                  height: 30,
                 ),
                 Container(
-                    padding:  const EdgeInsets.symmetric(vertical: 3),
+                    padding: const EdgeInsets.symmetric(vertical: 3),
                     decoration: BoxDecoration(
                       color: Colors.red.shade50,
                       borderRadius: BorderRadius.circular(7),
@@ -72,17 +78,19 @@ class LoginScreen extends StatelessWidget {
                           enabledBorder: InputBorder.none,
                           disabledBorder: InputBorder.none,
                           hintText: "Phone Number",
-                          hintStyle: TextStyle(color: Colors.blueGrey, fontSize: 16,
-                            fontWeight: FontWeight.w400,),
-                          contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14)
-                      ),
-                    )
-                ),
+                          hintStyle: TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 14)),
+                    )),
                 const SizedBox(
-                  height:20,
+                  height: 20,
                 ),
                 Container(
-                    padding:  const EdgeInsets.symmetric(vertical: 3),
+                    padding: const EdgeInsets.symmetric(vertical: 3),
                     decoration: BoxDecoration(
                       color: Colors.red.shade50,
                       borderRadius: BorderRadius.circular(7),
@@ -94,48 +102,54 @@ class LoginScreen extends StatelessWidget {
                           enabledBorder: InputBorder.none,
                           disabledBorder: InputBorder.none,
                           hintText: "Password",
-                          hintStyle: TextStyle(color: Colors.blueGrey, fontSize: 16,
-                            fontWeight: FontWeight.w400,),
-                          contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14)
-                      ),
-                    )
-                ),
+                          hintStyle: TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 14)),
+                    )),
                 const SizedBox(
-                  height:10,
+                  height: 10,
                 ),
                 RichText(
                     text: TextSpan(
-                      style: defaultStyle,
-                      children: [
-                        TextSpan(text:"Forgot Password?",
-                          style: linkStyle,
-                          recognizer: TapGestureRecognizer()..onTap=(){
-                            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder:(context)=>const RegisterScreen()), (route)=>true);
-                          },
-                        ),
-                      ],
-                    )),
+                  style: defaultStyle,
+                  children: [
+                    TextSpan(
+                      text: "Forgot Password?",
+                      style: linkStyle,
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const ForgotPassword()),
+                              (route) => true);
+                        },
+                    ),
+                  ],
+                )),
                 const SizedBox(
-                  height:100,
+                  height: 100,
                 ),
                 SizedBox(
                   height: 50,
                   width: MediaQuery.of(context).size.width,
                   child: ElevatedButton(
-                    onPressed: (){
-                      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder:(context)=>HomeScreen()), (route)=>true);
-
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomeScreen()),
+                          (route) => true);
                     },
                     style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty
-                          .all<Color>(Color(0xff4A148C)),
-                      shape: MaterialStateProperty
-                          .all<
-                          RoundedRectangleBorder>(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Color(0xff4A148C)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                         RoundedRectangleBorder(
-                            borderRadius: BorderRadius
-                                .circular(7)
-                        ),
+                            borderRadius: BorderRadius.circular(7)),
                       ),
                     ),
                     child: const Text(
